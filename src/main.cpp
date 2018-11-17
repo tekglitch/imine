c9f2d026a3bf283ebcaf39c753f7498834a4f4f4
tek <teksatan.420@gmail.com>
2018-11-17 07:28:19 -0500

    tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 void test(int fd, uint8_t type)
 {
-    std::cout << "word^" << std::endl;
+
 }
 
 void cltest(int fd, uint8_t type)
@@ -21,20 +21,20 @@ int main(int argc, const char *argv[])
 {
     try
     {
-    std::unique_ptr<imine::Engine> im = std::make_unique<imine::Engine>();
-    /// parse commandline arguments
+        std::unique_ptr<imine::Engine> im = std::make_unique<imine::Engine>();
+        /// parse commandline arguments
 
-    imine::read_config();
+        imine::read_config();
 
 
 
-    if(argc>0)
-    {
+        if(argc>0)
+        {
 
-    }
+        }
 
-    im->create_server(5555,test,cltest);
-    im->run();
+        im->create_server(5555,test,cltest);
+        im->run();
     }
     catch(const std::runtime_error &e)
     {
